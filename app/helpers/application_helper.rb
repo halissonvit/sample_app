0afd21db4cd93660b22eb4b8a817b796530ad314
Halisson Bruno Vitor <halissonvit@gmail.com>
2012-08-13 21:37:49 -0300

    Refactoring title in the pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title page_title
+    title = "Ruby on Rails Tutorial Sample App"
+    title = "#{title} | #{page_title}" unless page_title.nil? or page_title.empty?
+    title
+  end
 end
